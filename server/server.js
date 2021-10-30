abfab814d418a51276a6cf4a8378e752b3547ef0
Om Patil <200010036@iitdh.ac.in>
2021-10-30 19:49:21 +0530

    Quick Fix

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.set('trust proxy', 1)
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false, cookie: { secure: auto } }))
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false, cookie: { secure: 'auto' } }))
 app.use(passport.initialize())
 app.use(passport.session())
 
