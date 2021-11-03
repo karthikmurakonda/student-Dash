9ea070f07bc2e2980372ae58c93c831e65bd83b2
Om Patil <200010036@iitdh.ac.in>
2021-11-03 14:51:16 +0530

    Cleanup

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -19,7 +19,7 @@ export default function Signup() {
 	function checkUsername(username) {
 		return auth.usernameExists(username)
 	}
-	
+
 	function makeUser(values) {
 		auth.register(values.fname, values.lname, values.email, values.username, values.password);
 	}
@@ -36,14 +36,14 @@ export default function Signup() {
 								<Form.Group className="mb-3">
 									<Row>
 										<Col>
-											<FloatingLabel className="text-muted" label="First Name">
-												<Form.Control type="text" name="fname" value={values.fname} onChange={handleChange} onBlur={handleBlur} isInvalid={touched.fname && errors.fname} placeholder="First name"></Form.Control>
+											<FloatingLabel label="First Name">
+												<Form.Control type="text" name="fname" value={values.fname} onChange={handleChange} onBlur={handleBlur} isInvalid={touched.fname && errors.fname}></Form.Control>
 												<Form.Control.Feedback type="invalid">{errors.fname}</Form.Control.Feedback>
 											</FloatingLabel>
 										</Col>
 										<Col>
-											<FloatingLabel className="text-muted" label="Last Name">
-												<Form.Control type="text" name="lname" value={values.lname} onChange={handleChange} onBlur={handleBlur} isInvalid={touched.lname && !!errors.lname} placeholder="Last name"></Form.Control>
+											<FloatingLabel label="Last Name">
+												<Form.Control type="text" name="lname" value={values.lname} onChange={handleChange} onBlur={handleBlur} isInvalid={touched.lname && !!errors.lname}></Form.Control>
 												<Form.Control.Feedback type="invalid">{errors.lname}</Form.Control.Feedback>
 											</FloatingLabel>
 										</Col>
@@ -51,15 +51,15 @@ export default function Signup() {
 								</Form.Group>
 
 								<Form.Group className="mb-3">
-									<FloatingLabel className="text-muted mb-3" label="Email">
-										<Form.Control type="email" name="email" value={values.email} onChange={handleChange} onBlur={handleBlur} isValid={touched.email && !errors.email} isInvalid={touched.email && errors.email} placeholder="Institute Email"></Form.Control>
+									<FloatingLabel label="Email">
+										<Form.Control type="email" name="email" value={values.email} onChange={handleChange} onBlur={handleBlur} isValid={touched.email && !errors.email} isInvalid={touched.email && errors.email}></Form.Control>
 										<Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
 									</FloatingLabel>
 								</Form.Group>
 
 								<Form.Group className="mb-3">
-									<FloatingLabel className="text-muted" label="Username">
-										<Form.Control type="text" name="username" value={values.username} onChange={handleChange} onBlur={handleBlur} isValid={touched.username && !errors.username} isInvalid={touched.username && errors.username} placeholder="Username"/>
+									<FloatingLabel label="Username">
+										<Form.Control type="text" name="username" value={values.username} onChange={handleChange} onBlur={handleBlur} isValid={touched.username && !errors.username} isInvalid={touched.username && errors.username}/>
 										<Form.Control.Feedback type="invalid">
 											{errors.username}
 										</Form.Control.Feedback>
@@ -67,15 +67,15 @@ export default function Signup() {
 								</Form.Group>
 
 								<Form.Group className="mb-3">
-									<FloatingLabel className="text-muted" label="Password">
-										<Form.Control type="password" name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} isValid={touched.password && !errors.password} isInvalid={touched.password && errors.password} placeholder="Password"/>
+									<FloatingLabel label="Password">
+										<Form.Control type="password" name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} isValid={touched.password && !errors.password} isInvalid={touched.password && errors.password}/>
 										<Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
 									</FloatingLabel>
 								</Form.Group>
 
 								<Form.Group className="mb-3">
-									<FloatingLabel className="text-muted" label="Confirm Password">
-										<Form.Control type="password" name="confirm" value={values.confirm} onChange={handleChange} onBlur={handleBlur} isValid={touched.confirm && !errors.confirm} isInvalid={touched.confirm && errors.confirm} placeholder="Confirm Password"/>
+									<FloatingLabel label="Confirm Password">
+										<Form.Control type="password" name="confirm" value={values.confirm} onChange={handleChange} onBlur={handleBlur} isValid={touched.confirm && !errors.confirm} isInvalid={touched.confirm && errors.confirm} />
 										<Form.Control.Feedback type="invalid">{errors.confirm}</Form.Control.Feedback>
 									</FloatingLabel>
 								</Form.Group>
