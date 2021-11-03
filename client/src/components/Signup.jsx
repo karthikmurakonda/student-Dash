f2135d85c039a47117c5b40f6a3f0a72b8966975
Om Patil <200010036@iitdh.ac.in>
2021-11-03 14:56:11 +0530

    Cleanup

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 import { useAuth } from '../hooks/AuthContext'
 import { Form, Button, Container, Row, Col, FloatingLabel } from 'react-bootstrap'
 import * as yup from 'yup'
