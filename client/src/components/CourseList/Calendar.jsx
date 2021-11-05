88468c4831da6ed299520bf3d92519d7d7de51de
Om Patil <200010036@iitdh.ac.in>
2021-11-05 11:57:15 +0530

    Format calendar

--- client/src/components/CourseList/Calendar.jsx
+++ client/src/components/CourseList/Calendar.jsx
@@ -4,11 +4,17 @@ import 'tui-calendar/dist/tui-calendar.css';
 // make page that has tui calendar in the left and a list of courses in the right
 // when you click on a course, it should show course in calendar
 
+var templates = {
+    weekDayname: function(model) {
+        return '<span class="fs-4">' + model.dayName + '</span>';
+    }
+  }
+
 function Timetable(props) {
   const { courses } = props;
     return (
       <div>
-        <Calendar/>
+        <Calendar taskView={false} scheduleView={['time']} template={templates} week={{hourStart:7, hourEnd:20}} />
       </div>
     );
 }
