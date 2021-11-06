b1f2ac94066ab0fd0bd50dc0b6d52fd5cbdc5719
Om Patil <200010036@iitdh.ac.in>
2021-11-06 14:56:17 +0530

    New bootstrap layout

--- client/src/components/CourseList/Courseplanner.jsx
+++ client/src/components/CourseList/Courseplanner.jsx
@@ -1,9 +1,13 @@
 import Timetable from "./Calendar";
 import CourseList from "./CourseList";
 import "./style.css";
+import { Container, Row, Col, Collapse, Card } from 'react-bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import { useState } from "react";
 
 //render the component by function
 export default function Courseplanner() {
+    const [showCoursesi, setShowCoursesi] = useState(false)
 
     // dummy props
     const courses = [
@@ -20,22 +24,30 @@ export default function Courseplanner() {
             course: "Node",
         },
     ];
-
     return (
-        <div className="courseplanner">
-        <div className="courseplanner-header">
-            <h1>Courseplanner</h1>
-        </div>
-        <div className="timetable-wrapper">
-        <Timetable />
-
-        </div>
-        <div className="courseplanner-body">
-            <CourseList
-                courses={courses}
-             />
-        </div>
-        </div>
+        <Container fluid>
+            <Row>
+                <Col>
+                    <h1>Courseplanner</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col lg='10'>
+                    <Timetable />
+                </Col>
+                <Col lg='2' className='px-3'>
+                    <h3>Courses <a href="javascript:void(0)" onClick={() => setShowCoursesi(!showCoursesi)} class="link-primary h5"><i class="bi bi-info-circle"></i></a></h3>
+                    <Collapse in={showCoursesi}>
+                        <Card>
+                            <Card.Body>
+                                Select the courses you wish to take! Clashing or uneligible courses will be disabled.
+                            </Card.Body>
+                        </Card>
+                    </Collapse>
+                    <CourseList courses={courses} />
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
