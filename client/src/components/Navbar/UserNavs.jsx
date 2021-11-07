eb494b6761b95ba3e90489cf512b2404b31e51cf
Om Patil <200010036@iitdh.ac.in>
2021-11-07 17:39:47 +0530

    Highlight current page

--- client/src/components/Navbar/UserNavs.jsx
+++ client/src/components/Navbar/UserNavs.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { useAuth } from '../../hooks/AuthContext'
 import { Nav } from 'react-bootstrap'
+import { useLocation } from 'react-router-dom'
 
 export default function UserNavs() {
     const auth = useAuth()
+    const location = useLocation()
+    console.log(location.pathname);
 
     if (auth.user) {
         return (
             < >
-            <Nav className="me-auto">
-                <Nav.Link href="courseplanner">Course Planner</Nav.Link>
+            <Nav activeKey={location.pathname} className="me-auto">
+                <Nav.Link href="/courseplanner">Course Planner</Nav.Link>
             </Nav>
             </>
         )
