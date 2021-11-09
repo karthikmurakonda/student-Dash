9be50763c30073e90255ae65b743951ff62e2b42
Om Patil <200010036@iitdh.ac.in>
2021-11-09 22:09:32 +0530

    Added pagination to posts

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react'
-import { Alert, Col, Container, Row, Button, Modal } from 'react-bootstrap'
+import { Alert, Col, Container, Row, Button, Modal, Pagination } from 'react-bootstrap'
 import axios from 'axios'
 import Post from './Post'
 import Loading from '../Loading/Loading'
@@ -15,18 +15,35 @@ export default function Posts() {
     const [loaded, setLoaded] = useState(false)
     const [showModal, setShowModal] = useState(false)
     const [update, setUpdate] = useState(false)
+    const [items, setItems] = useState([])
+    const [pages, setPages] = useState(1)
+    const [page, setPage] = useState(1)
 
     useEffect(() => {
-        server.get("/")
+        server.get("/", { params: { page: page } })
             .then((res) => {
                 setPosts(res.data.results)
                 setLoaded(true)
+                setPage(res.data.page)
+                setPages(res.data.totalPages)
             })
             .catch((err) => {
                 console.log(err)
                 setLoaded(true)
             })
-    }, [update])
+    }, [update, page])
+
+    useEffect(() => {
+        let items = [];
+        for (let number = 1; number <= pages; number++) {
+            items.push(
+                <Pagination.Item key={number} active={number === page} onClick={()=>setPage(number)}>
+                {number}
+                </Pagination.Item>,
+            );
+        }
+        setItems(items)
+    }, [page, pages])
 
     if (loaded) {
         return (
@@ -35,7 +52,7 @@ export default function Posts() {
                 <Row className="justify-content-center">
                     <Col lg={6}>
                         <Row className="justify-content-end">
-                            <Col />
+                            <Col><h3>Recent Posts</h3></Col>
                             <Col className="text-end">
                                 <Button variant="primary" onClick={()=>setShowModal(true)} >Create Post</Button>
                             </Col>
@@ -45,6 +62,7 @@ export default function Posts() {
                         )) : (
                             <Alert variant="info" className="text-center">No posts here!</Alert>
                         )}
+                        <Pagination>{items}</Pagination>
                     </Col>
                 </Row>
             </Container>
